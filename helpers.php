0491b17240b9009be1d363da572bcd07692a0a79
Asier Marqués <asiermarques@gmail.com>
2016-11-13 14:47:41 +0100

    helpers in autoload

--- helpers.php
+++ helpers.php
@@ -5,11 +5,22 @@
  * Time: 14:43
  */
 
+
+
+/**
+ * @return object \Simettric\Sense\View\View
+ */
 function sense_view()
 {
     return Simettric\Sense\Kernel::getInstance()->getContainer()->get("view");
 }
 
+/**
+ * @param $route_name
+ * @param array $params
+ * @param bool $absolute
+ * @return mixed
+ */
 function sense_url($route_name, $params=[], $absolute=false)
 {
 
